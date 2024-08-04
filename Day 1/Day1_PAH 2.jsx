edcc7378ba6e3f55c8f3933fe9fcb84c5eb1bb1a
murugaprakash-133 <727723eucs133@skcet.ac.in>
2024-08-04 16:59:51 +0530

    Update Day1_PAH 2.jsx

--- Day 1/Day1_PAH 2.jsx
+++ Day 1/Day1_PAH 2.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
-const display = () => {
-    alert("Check the console output!");
-    let age = 18;
-    let name = 'Kiran';
-    let setStatus = false;
-    let userDefault = undefined;
-    let responseValue = null;
-
-    console.log(age);
-    console.log(name);
-    console.log(setStatus);
-    console.log(userDefault);
-    console.log(responseValue);
-};
-
 const App = () => {
+    const display = () => {
+        alert("Check the console output!");
+        let age = 18;
+        let name = 'Kiran';
+        let setStatus = false;
+        let userDefault = undefined;
+        let responseValue = null;
+    
+        console.log(age);
+        console.log(name);
+        console.log(setStatus);
+        console.log(userDefault);
+        console.log(responseValue);
+    };
+    
     return (
         <>
             <button onClick={display}>Click me!</button>
