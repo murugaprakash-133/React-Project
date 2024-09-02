7e54a20d5b3ca47cf7a9c18e690d49b13e7eb93c
murugaprakash-133 <727723eucs133@skcet.ac.in>
2024-09-02 13:10:24 +0530

    Add files via upload

---
+++ Day 2/Day2_DC.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const App = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(name);
+        console.log(email);
+        console.log(address);
+        console.log(message);
+
+        setName('');
+        setEmail('');
+        setAddress('');
+        setMessage('');
+    } 
+    return (
+        <div style={{display:'flex',
+            flexDirection:'column',
+            justifyContent:'center',
+            alignItems: 'center',
+            height:'100vh',
+            textAlign:'center',
+        }}>
+            <form action={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required/>
+                </div>
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                </div>
+                <div>
+                    <label htmlFor="address">Address: </label>
+                    <textarea id="address" value={address} onChange={(e) => setAddress(e.target.value)} required/>
+                </div>
+                <div>
+                    <label htmlFor="message">Message: </label>
+                    <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} required/>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default App;
